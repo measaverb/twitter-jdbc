100c5fe6b413091c2d1e72af40cc3ee885f1ef5e
measaverb <measaverb@gachon.ac.kr>
2025-11-29 01:21:42 +0900

    Add User model class with properties and constructor

---
+++ src/com/twitter/model/User.java
@@ -0,0 +1,21 @@
+package com.twitter.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
